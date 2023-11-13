cf6239719b0df881914e2edcbc7b500e6d2f68c2
Derrick Weis <derrick@derrickweis.com>
2023-11-12 20:49:27 -0800

    Initial commit based on https://github.com/srid/rust-nix-template but modifying for my own purposes.

---
+++ README.md
@@ -0,0 +1,1 @@
+A rust template that's suitable for my projects. Originally based on [srid's template here](https://github.com/srid/rust-nix-template)
---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    systems.url = "github:nix-systems/default";
+    # Dev tools
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = { self, flake-parts, rust-overlay, ... }@ inputs:
+    let
+      name = "rust-nix-template";
+    in
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = { config, self', pkgs, lib, system, rust-overlay, ... }:
+        let
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          nonRustDeps = [
+            pkgs.libiconv
+          ];
+          rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./toolchain.toml;
+        in
+        {
+          _module.args.pkgs = import self.inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import self.inputs.rust-overlay) ];
+          };
+
+          # Rust package
+          packages.default = pkgs.rustPlatform.buildRustPackage {
+            inherit (cargoToml.package) name version;
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+
+          # Rust dev environment
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.treefmt.build.devShell
+            ];
+            shellHook = ''
+              # For rust-analyzer 'hover' tooltips to work.
+              export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
+
+              echo
+              echo "🦀 Run 'just <recipe>' to get started 🦀"
+              just
+            '';
+            buildInputs = nonRustDeps;
+            nativeBuildInputs = with pkgs; [
+              just
+              rustToolchain
+              cargo-watch
+              rust-analyzer
+            ];
+            RUST_BACKTRACE = 1;
+          };
+
+          # Add your auto-formatters here.
+          # cf. https://numtide.github.io/treefmt/
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+        };
+    };
+}
